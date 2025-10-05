a42be9278b5b61c20e8cac37209dd6ad394f3fbb
Francis <fnkorley@gmail.com>
2025-10-05 11:14:21 +0100

    Fix: rename test_multiply to test_multiplication for checker compatibility

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -24,7 +24,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
         self.assertEqual(self.calc.subtract(10, 0), 10)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
